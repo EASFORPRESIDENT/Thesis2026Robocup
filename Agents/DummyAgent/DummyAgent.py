7aae93110716f8c990b4f7c6990f97a57fa5ec26
Andreas <andreas@pearson-robotics.com>
2026-03-20 11:12:11 +0100

    Added dummy agent

---
+++ Agents/DummyAgent/DummyAgent.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import hfo
+import itertools
+import random
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+HFO_ROOT = PROJECT_ROOT.parent / "HFO"
+FORMATIONS_PATH = HFO_ROOT / "bin/teams/base/config/formations-dt"
+
+def main():
+    env = hfo.HFOEnvironment()
+    env.connectToServer(
+        hfo.HIGH_LEVEL_FEATURE_SET, 
+        str(FORMATIONS_PATH), 
+        6000, 
+        'localhost', 
+        'base_left', 
+        False
+    )
+
+    my_unum = env.getUnum()
+    teammate_unums = [u for u in range(1, 12) if u != my_unum]
+    
+    for episode in itertools.count():
+        status = hfo.IN_GAME
+        while status == hfo.IN_GAME:
+            state = env.getState()
+
+            if state[5] == 1:
+                if random.random() < 0.5:
+                    env.act(hfo.SHOOT)
+                else:
+                    #env.act(hfo.PASS, random.choice(teammate_unums))
+                    env.act(hfo.DRIBBLE)
+            else:
+                env.act(hfo.MOVE)
+            
+            status = env.step()
+
+        print(f"Episode {episode} ended with {env.statusToString(status)}")
+
+        if status == hfo.SERVER_DOWN:
+            env.act(hfo.QUIT)
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ Agents/README.md
@@ -0,0 +1,16 @@
+# Agents playing HFO
+
+## HFO interpreter for VS Code
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install numpy
+python -m pip install /absolute/path/to/HFO
+```
+
+In VS Code, in Thesis2026Robocup folder:
+* Press ```Ctrl+Shift+P```
+* Run Python: Select Interpreter
+* Choose ```Thesis2026Robocup/.venv/bin/python```
