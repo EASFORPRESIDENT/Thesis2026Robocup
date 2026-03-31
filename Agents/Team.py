0c87b63e65ad1cbd4f54b619d04dea45d2f77f54
Mattias Tidström <mtm20001@student.mdu.se>
2026-03-31 11:46:46 +0200

    teams fix

--- Agents/Team.py
+++ Agents/Team.py
@@ -13,11 +13,15 @@ import argparse
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Team controller")
 
-    parser.add_argument("--n-agents", type=int, default=2,
+    parser.add_argument("--n-O_agents", type=int, default=2,
                         help="Number of offensive agents")
+    
+    parser.add_argument("--n-D_agents", type=int, default=2,
+                        help="Number of defensive agents")
 
     #parser.add_argument("--port", type=int, default=6000, help="HFO server port")
 
@@ -27,12 +31,14 @@ def parse_args():
 
 def main():
     args = parse_args()
-    OBS_DIM = 20 
-    STATE_DIM = 50
-    N_AGENTS= args.n_agents
-    N_ACTIONS=8
-    model = QMIX(OBS_DIM,STATE_DIM,N_AGENTS,N_ACTIONS)
+    OBS_DIM = 10 + 6*(args.n_O_agents-1) + 3*args.n_D_agents
+    STATE_DIM = OBS_DIM*args.n_O_agents #kolla upp om kan använda motståndare obs också
+    N_AGENTS= args.n_O_agents
+    N_ACTIONS= 10 + (args.n_O_agents-1)
+    WEIGHTS_PATH = PROJECT_ROOT / f"Agents/Agent/{args.n_O_agents}v{args.n_D_agents}.pt"
 
+    model = QMIX(OBS_DIM,STATE_DIM,N_AGENTS,N_ACTIONS)
+    torch.save(model.agent_net.state_dict(), WEIGHTS_PATH)
 
     processes = []
 
@@ -40,8 +46,8 @@ def main():
         p = subprocess.Popen([
             sys.executable,
             "Agents/Agent/Agent.py",
-            "--agent-id",
-            str(i)
+            "--agent-id", str(i),
+            "--weights-path", str(WEIGHTS_PATH)
         ])
         processes.append(p)
         time.sleep(3) #Need this to avoid race condition
