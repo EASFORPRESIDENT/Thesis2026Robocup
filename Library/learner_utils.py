21bb019e391fdbd0122432701ae953972555554a
Andreas Pearson <andreas@pearson-robotics.com>
2026-04-03 15:17:11 +0200

    Fix tensor dimension ordering in collate_batch function

--- Library/learner_utils.py
+++ Library/learner_utils.py
@@ -29,6 +29,9 @@ def collate_batch(batch):
         actions.append(sample_actions)
 
     obs = torch.tensor(obs, dtype=torch.float32)
+    obs = obs.permute(1, 0, 2, 3)
     states = torch.tensor(states, dtype=torch.float32)
+    states = states.permute(1, 0, 2)
     actions = torch.tensor(actions, dtype=torch.int64)
+    actions = actions.permute(1, 0, 2)
     return obs, states, actions
